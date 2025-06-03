9f98b9dfba5f071c9baa4330145ac0009dd90ae0
Sameerdevlo <sam884156@gmail.com>
2025-06-03 15:23:37 +0530

    update

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin',
-    ['module:react-native-dotenv']
-  ],
-  plugins: [
     ['module:react-native-dotenv', {
       "moduleName": "@env",
       "path": ".env",
       "allowUndefined": true
-    }]
-  ]
+    }],
+  ],
+  plugins: ['react-native-reanimated/plugin'], 
 };

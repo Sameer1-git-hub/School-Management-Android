2039552a51a8a94b3535bcb61a1c72fe1ffc4453
Sameerdevlo <sam884156@gmail.com>
2025-06-10 20:18:41 +0530

    update

--- Navigators/AdminStack.js
+++ Navigators/AdminStack.js
@@ -1,21 +1,35 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Home from '../Screens/Home'
-import Attendance from '../Screens/AdminScreens/Attendance'
-import News from '../Screens/Common/News'
-import NewsDetails from '../Screens/Common/NewsDetails'
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../Screens/Home';
+import Attendance from '../Screens/AdminScreens/Attendance';
+import News from '../Screens/Common/News';
+import NewsDetails from '../Screens/Common/NewsDetails';
 const AdminStack = () => {
-    const Authorized = createNativeStackNavigator()
-    return (
-        <Authorized.Navigator initialRouteName='HomeScreen'>
+  const Authorized = createNativeStackNavigator();
+  return (
+    <Authorized.Navigator initialRouteName="HomeScreen">
+      <Authorized.Screen
+        name="HomeScreen"
+        component={Home}
+        options={{headerShown: false}}
+      />
+      <Authorized.Screen
+        name="Attendance"
+        component={Attendance}
+        options={{headerShown: false}}
+      />
+      <Authorized.Screen
+        name="News"
+        component={News}
+        options={{headerShown: false}}
+      />
+      <Authorized.Screen
+        name="NewsDetails"
+        component={NewsDetails}
+        options={{headerShown: false}}
+      />
+    </Authorized.Navigator>
+  );
+};
 
-            <Authorized.Screen name="HomeScreen" component={Home} options={{ headerShown: false }} />
-            <Authorized.Screen name="Attendance" component={Attendance} options={{ headerShown: false }} />
-            <Authorized.Screen name='News' component={News} options={{ headerShown: false }} />
-            <Authorized.Screen name='NewsDetails' component={NewsDetails} options={{ headerShown: false }} />
-
-        </Authorized.Navigator>
-    )
-}
-
-export default AdminStack
+export default AdminStack;
--- Navigators/Drawer.js
+++ Navigators/Drawer.js
@@ -1,83 +1,83 @@
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import MyProfile from '../Screens/DrawerScreens/MyProfile';
 import TeacherStack from './TeacherStack';
 import StudentStack from './StudentStack';
 import AdminStack from './AdminStack';
-import HomeIcon from "react-native-vector-icons/AntDesign"
+import HomeIcon from 'react-native-vector-icons/AntDesign';
 import CustomDrawer from '../Components/Drawer/CustomDrawer';
 import Settings from '../Screens/DrawerScreens/Settings';
 import LoadingScreen from '../Components/Home/LoadingScreen';
-import { ImageBackground, StyleSheet, Text, View, TextInput, Dimensions, TouchableOpacity, ScrollView } from 'react-native'
-
-
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
-const MyDrawer = ({ role, isLoading }) => {
+const MyDrawer = ({role, isLoading}) => {
+  if (isLoading) return <LoadingScreen />;
 
-    if (isLoading) return <LoadingScreen />
+  const StackByRole = () => {
+    if (role === 'super admin') return <AdminStack />;
+    if (role === 'admin') return <AdminStack />;
+    if (role === 'teacher') return <TeacherStack />;
+    if (role === 'student') return <StudentStack />;
+  };
 
-    const StackByRole = () => {
-        if (role === "super admin")
-            return <AdminStack />
-        if (role === "admin")
-            return <AdminStack />
-        if (role === "teacher")
-            return <TeacherStack />
-        if (role === "student")
-            return <StudentStack />
-    }
+  // const StackByRole = () => {
+  //         if (role === "super admin") return <AdminStack />;
+  //         if (role === "admin") return <AdminStack />;
+  //         if (role === "teacher") return <TeacherStack />;
+  //         if (role === "student") return <StudentStack />;
+  //         return <Text>No valid role</Text>;
+  //     };
+  console.log('Drawer Role:', role);
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerContent={props => <CustomDrawer {...props} />}
+      screenOptions={{
+        drawerStyle: {width: '60%'},
+        drawerLabelStyle: {marginLeft: -25, color: 'black'},
+        drawerActiveBackgroundColor: '#D5EEFB',
+        drawerActiveTintColor: 'black',
+        headerShown: false,
+      }}>
+      <Drawer.Screen
+        name="Home"
+        component={StackByRole}
+        options={{
+          drawerIcon: () => <HomeIcon name="home" size={20} color="black" />,
+        }}
+      />
 
-// const StackByRole = () => {
-//         if (role === "super admin") return <AdminStack />;
-//         if (role === "admin") return <AdminStack />;
-//         if (role === "teacher") return <TeacherStack />;
-//         if (role === "student") return <StudentStack />;
-//         return <Text>No valid role</Text>;
-//     };
-console.log("Drawer Role:", role)
-    return (
-        <Drawer.Navigator
-            initialRouteName="Home"
-            drawerContent={props => <CustomDrawer {...props} />}
-            screenOptions={{
-                drawerStyle: { width: "60%" },
-                drawerLabelStyle: { marginLeft: -25, color: 'black' },
-                drawerActiveBackgroundColor: '#D5EEFB',
-                drawerActiveTintColor: "black",
-                headerShown: false,
-            }}>
+      <Drawer.Screen
+        name="Profile"
+        component={MyProfile}
+        options={{
+          drawerIcon: () => <HomeIcon name="user" size={20} color="black" />,
+        }}
+      />
 
-            <Drawer.Screen
-                name="Home"
-                component={StackByRole}
-                options={{
-                    drawerIcon: () => (
-                        <HomeIcon name="home" size={20} color="black" />
-                    )
-                }} />
-
-            <Drawer.Screen
-                name="Profile"
-                component={MyProfile} options={{
-                    drawerIcon: () => (
-                        <HomeIcon name="user" size={20} color="black" />
-                    )
-                }} />
-
-            <Drawer.Screen
-                name="Change Password"
-                component={Settings}
-                options={{
-                    drawerIcon: () => (
-                        <HomeIcon name="setting" size={20} color="black" />
-                    )
-                }} />
-
-
-
-        </Drawer.Navigator>
-    );
-}
+      <Drawer.Screen
+        name="Change Password"
+        component={Settings}
+        options={{
+          drawerIcon: () => <HomeIcon name="setting" size={20} color="black" />,
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 export default MyDrawer;
